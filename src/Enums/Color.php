11a660f9de6c4a45e2cfb7d042b3591eaac32fd2
Kornberger <k98kornr@int.kkh-services.local>
2024-10-08 19:38:35 +0200

    Create Color.php

---
+++ src/Enums/Color.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rakoitde\Table\Enums;
+
+enum Color
+{
+    public const Primary          = 'primary';
+    public const Secondary        = 'secondary';
+    public const Success          = 'success';
+    public const Danger           = 'danger';
+    public const Warning          = 'warning';
+    public const Info             = 'info';
+    public const Light            = 'light';
+    public const Dark             = 'dark';
+    public const Link             = 'link';
+    public const OutlinePrimary   = 'outline-primary';
+    public const OutlineSecondary = 'outline-secondary';
+    public const OutlineSuccess   = 'outline-success';
+    public const OutlineDanger    = 'outline-danger';
+    public const OutlineWarning   = 'outline-warning';
+    public const OutlineInfo      = 'outline-info';
+    public const OutlineLight     = 'outline-light';
+    public const OutlineDark      = 'outline-dark';
+}
