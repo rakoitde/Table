96001525949e33646329c192d9e08d9b3399f38f
Kornberger, Ralf <Ralf.Kornberger@kk-service.de>
2024-10-15 12:31:09 +0200

    feat: add filter visability

---
+++ src/Filters/Traits/hasVisability.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rakoitde\Table\Filters\Traits;
+
+trait hasVisability
+{
+    protected bool $isVisible = true;
+    protected bool $toggleable;
+    protected bool $shown;
+
+    public function visible(bool $isVisible = true): self
+    {
+        $this->isVisible = $isVisible;
+
+        return $this;
+    }
+
+    public function isVisible()
+    {
+        return true === $this->isVisible;
+    }
+}
