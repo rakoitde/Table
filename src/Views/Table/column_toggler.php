f4e0c82b7a0d51746503e082ab3351cf72cfe8df
Kornberger, Ralf <Ralf.Kornberger@kk-service.de>
2024-10-15 10:30:27 +0200

    feat: add show all columns button

--- src/Views/Table/column_toggler.php
+++ src/Views/Table/column_toggler.php
@@ -1,4 +1,4 @@
-                    <?php if ($Table->hasToggleableColumns()) : ?>
+<?php if ($Table->hasToggleableColumns()) : ?>
                     <!-- Columns Toggler -->
                     <div class="btn-group">
                         <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-label="Columns" title="Columns" aria-expanded="true">
@@ -9,7 +9,7 @@
 
                                 <div class="d-flex justify-content-between">
                                     <span class="fs-6 fw-bold"><i class="bi bi-list-ul pe-2"></i>Spalten</span>
-                                    <span><a href="<?= base_url() ?>" class="text-decoration-none text-danger">showAll</a></span>
+                                    <span><button href="<?= base_url() ?>" class="btn btn-sm btn-link fw-bold text-danger text-decoration-none text-nowrap" id="<?= $Table->getId().'_toggler_show_all_button' ?>">alle anzeigen</b></span>
                                 </div>
 
                                 <div><hr class="dropdown-divider"></div>
@@ -19,47 +19,66 @@
                                     name="table[<?= $Table->getId() ?>][toggled]"
                                     value="1">
 
-                                <!-- Columns -->
+                                <!-- toggleable columns -->
+                                <div id="<?= $Table->getId() ?>_toggler_toggleable_columns">
                                 <?php foreach ($Table->getColumns() as $column) : ?>
                                     <?php if($column->isVisible() && $column->isToggleable()) : ?>
-                                    <div class="row mb-1">
-                                        <label>
+                                        <div class="form-check">
                                             <input
-                                                type="checkbox"
-                                                data-field="<?= $column->getField() ?>"
-                                                <?= $column->getToggleCheckedAttribute() ?>
-                                                name="table[<?= $Table->getId() ?>][toggle][<?= $column->getField() ?>]"
-                                                form="form_<?= $Table->getId() ?>"
-                                                value="1"
-                                                data-column="<?= $Table->getId() ?>_column_<?= $column->getField() ?>">
-                                            <span><?= $column->getLabel() ?></span>
-                                        </label>
-                                    </div>
+                                                    class="form-check-input"
+                                                    type="checkbox"
+                                                    data-field="<?= $column->getField() ?>"
+                                                    <?= $column->getToggleCheckedAttribute() ?>
+                                                    name="table[<?= $Table->getId() ?>][toggle][<?= $column->getField() ?>]"
+                                                    id="<?= $Table->getId() ?>_toggler_<?= $column->getField() ?>"
+                                                    form="form_<?= $Table->getId() ?>"
+                                                    value="1"
+                                                    data-column="<?= $Table->getId() ?>_column_<?= $column->getField() ?>">
+                                            <label class="form-check-label" for="<?= $Table->getId() ?>_toggler_<?= $column->getField() ?>">
+                                            <?= $column->getLabel() ?>
+                                            </label>
+                                        </div>
                                     <?php endif ?>
                                 <?php endforeach ?>
-
-                                <script>
-
-                                    function toggleColumnVisability(columnClass, isShown) {
-                                        let columns = document.getElementsByClassName(columnClass)
-                                        Array.prototype.forEach.call(columns, function (column) {
-                                            if (isShown) {
-                                                column.removeAttribute('hidden')
-                                            } else {
-                                                column.setAttribute('hidden', true)
-                                            }
-                                        });
-                                    }
-
-                                    let myTableColumns = document.getElementById('<?= $Table->getId() ?>_column_toggler').querySelectorAll('input[type=checkbox]')
-                                    myTableColumns.forEach(function (toggler) {
-                                        toggler.addEventListener('click', function() {
-                                            toggleColumnVisability(toggler.dataset.column, this.checked)
-                                        });
-                                    });
-                                </script>
-
+                                </div>
                             </div>
                         </div>
                     </div>
                     <?php endif ?>
+
+                    
+                    <script>
+
+                        function toggleColumnVisability(columnClass, isShown) {
+                            let columns = document.getElementsByClassName(columnClass)
+                            Array.prototype.forEach.call(columns, function (column) {
+                                if (isShown) {
+                                    column.removeAttribute('hidden')
+                                } else {
+                                    column.setAttribute('hidden', true)
+                                }
+                            });
+                        }
+
+                        let myTableColumns = document.getElementById('<?= $Table->getId() ?>_column_toggler').querySelectorAll('input[type=checkbox]')
+                        myTableColumns.forEach(function (toggler) {
+                            toggler.addEventListener('click', function() {
+                                toggleColumnVisability(toggler.dataset.column, this.checked)
+                            });
+                        });
+
+                        function showAllToggleableColumns()
+                        {
+                            let columnCheckboxes = document.querySelectorAll("#<?= $Table->getId() ?>_toggler_toggleable_columns input[type=checkbox]");
+                            columnCheckboxes.forEach(function (checkbox) {
+                                checkbox.checked = true;
+                                toggleColumnVisability(checkbox.dataset.column, checkbox.checked)
+                            }, this);
+                        }
+
+                        document.getElementById('<?= $Table->getId().'_toggler_show_all_button' ?>').addEventListener("click", function(event){
+                            event.preventDefault()
+                            showAllToggleableColumns()
+                        });
+
+                    </script>
