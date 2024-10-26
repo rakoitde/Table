0c0fd83dc2c48d488d1f087b66e77c50954360bd
Kornberger, Ralf <Ralf.Kornberger@kk-service.de>
2024-10-26 04:34:16 +0200

    ref: removed unneeded code

--- src/Table.php
+++ src/Table.php
@@ -171,39 +171,6 @@ class Table
     }
 
     /**
-     * if there are searchable columns, the model will be searched by the affected fields
-     */
-    public function searchModel()
-    {
-
-        if (! $this->hasSearchableFields) { return; }
-
-        $search = $this->getSearchString();
-        if ($search == '') { return; }
-
-        $search = trim(str_replace("*", "%", $search));
-        $segments = preg_split("/[\s,]+|[.]+/", $search);
-        
-        foreach ($segments as $segment) {
-
-            $this->model->groupStart();
-
-            foreach ($this->getColumns() as $column) {
-
-                if (! $column->isSearchable()) { continue; }; 
-                
-                foreach ($column->getSearchableFields() as $field) {
-
-                    $this->model->orLike($field, str_replace('*', '%', $segment));
-
-                }
-            }
-
-            $this->model->groupEnd();
-        }
-    }
-
-    /**
      * if there are sortable columns, the model will be sorted by the affected fields
      */
     public function sortModel()
